3db6249c5af731b6d54a249eefb0d8325be260fc
niwasaka-labellevie <niwasaka@labellevie.jp>
2025-04-04 11:10:42 +0900

    mysqlDB接続まで

---
+++ db/init-users.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(32) NOT NULL, email VARCHAR(32) NOT NULL);
+INSERT users (name, email) VALUES ("daihuku", "daihuku@example.com");
+INSERT users (name, email) VALUES ("ohagi", "ohagi@example.com");
